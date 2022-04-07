c85c6a9064d8c4a1232ea73220d751d668484f86
antoniopl6 <72573790+antoniopl6@users.noreply.github.com>
2022-04-07 23:05:41 +0200

    final

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pickup : MonoBehaviour
+{
+
+    // Before rendering each frame..
+    void Update()
+    {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# P1
+In BuildsWindows the release can be executed just with P1.exe
+
+P1 sistemas interactivos
+
+En BuildsWindows se puede ejecutar el juego, ejecutando el ejecutable P1.exe.
+
